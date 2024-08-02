98faaf283c0efa67b481db46273ce2b4f7329206
Ashraf1987 <learning.yahoo@gmail.com>
2024-08-02 16:56:14 +0200

    j

--- Udacity project/Adventur_game.py
+++ Udacity project/Adventur_game.py
@@ -2,221 +2,133 @@ import time
 import random
 
 
-### variables and randommes
-melee_weapon = random.choice(
-    ["Spirit sword", "Holy dagger", "Demonic chainsaw", "Demonic long sword"]
-)
-monster = random.choice(
-    ["Ghost", "Skelleton", " Zompie", "half demon", "impure vampire"]
-)
-color = random.choice(["Blue", "Black", "Red", "white", "purple"])
-ranged_weapons = random.choice(
-    [
-        " Holy arrows ",
-        " Fire cross bow ",
-        " ultrasonic waves ",
-        " Demonic shot guns",
-    ]
-)
-current_points = 30
-
-def End():
-     print_pause("Thanks for playing! Goodbye.")
-     print_pause("If you want to play again after some time enter yes")
-### Asks if player wants to play again
-def play_over():
-     while True:
-        choice = input("Would you like to play again? (yes/no): ").lower()
-        if choice == 'yes':
-            game_intro()
-            break
-        elif choice == 'no':
-          End()
-        else:
-          print("Invalid input. Please enter 'yes' or 'no'.")
+# Define global variables
+MELEE_WEAPONS = ["Spirit sword", "Holy dagger", "Demonic chainsaw", "Demonic long sword"]
+MONSTERS = ["Ghost", "Skeleton", "Zombie", "Half demon", "Impure vampire"]
+COLORS = ["Blue", "Black", "Red", "White", "Purple"]
+RANGED_WEAPONS = ["Holy arrows", "Fire crossbow", "Ultrasonic waves", "Demonic shotguns"]
+INITIAL_POINTS = 30
 
 
-### function to  to tell you how good you are in the game
-def points(current_points):
-    if current_points == 60:
-        print_pause("You clear the game")
-        print_pause("Oh my you are so good at this game")
-        play_over()
-    elif 0 < current_points < 50:
-        print_pause("You clear the game")
-        print_pause("You are gaming skills are pretty average ")
-        play_over()
-
-
-### Time.sleep function
-def print_pause(string: str):
-    print(string)
-    print("")
+def print_pause(message: str) -> None:
+    """Prints a message with a pause."""
+    print(message)
     time.sleep(1)
 
 
-### game intro
-def game_intro():
-    print_pause("current point =")
-    print_pause(current_points)
-    print_pause("Welcome to the ancient house")
-    print_pause("You are a Spirit hunter and you have been")
-    print_pause("tasked to purify A house of Its Dark spirits")
-    ### loop to handle invalid input
+def end_game() -> None:
+    """Ends the game and prompts to play again."""
+    print_pause("Thanks for playing! Goodbye.")
+    play_again = input("If you want to play again, enter 'yes': ").lower()
+    if play_again == "yes":
+        game_intro()
+    else:
+        print_pause("Goodbye!")
+
+
+def adjust_points(points: int, amount: int) -> int:
+    """Adjusts the current points and checks if the game is over."""
+    points += amount
+    if points <= 0:
+        print_pause("Game over. You suck at this game.")
+        end_game()
+    return points
+
+
+def game_intro() -> None:
+    """Introduces the game and starts the first room."""
+    melee_weapon = random.choice(MELEE_WEAPONS)
+    monster = random.choice(MONSTERS)
+    color = random.choice(COLORS)
+    ranged_weapon = random.choice(RANGED_WEAPONS)
+    current_points = INITIAL_POINTS
+
+    print_pause(f"Current points: {current_points}")
+    print_pause("Welcome to the ancient house.")
+    print_pause("You are a Spirit hunter tasked with purifying a house of its dark spirits.")
+
     while True:
-        intro = input("(Please enter 1 to enter first room)")
-        if intro == "1":
-            first_room(melee_weapon, color, monster, current_points)
+        choice = input("Enter '1' to enter the first room: ")
+        if choice == "1":
+            first_room(melee_weapon, color, monster, ranged_weapon, current_points)
             break
+
+
+def first_room(melee_weapon: str, color: str, monster: str, ranged_weapon: str, current_points: int) -> None:
+    """Handles the first room of the game."""
+    print_pause(f"You enter the first room and see a terrifying {color} {monster}.")
+    print_pause("Enter '1' to use your melee weapon to kill it or '2' to escape.")
+
+    while True:
+        choice = input("Choose an option (1 or 2): ")
+        if choice == "1":
+            print_pause(f"You tear the {monster} to shreds using {melee_weapon}.")
+            current_points += 10
+            print_pause(f"Current points: {current_points}")
+
+            while True:
+                next_room = input("Enter '1' to enter the second room: ")
+                if next_room == "1":
+                    second_room(melee_weapon, ranged_weapon, color, monster, current_points)
+                    break
+        elif choice == "2":
+            print_pause("You manage to escape but you were injured.")
+            current_points = adjust_points(current_points, -10)
+            print_pause(f"Current points: {current_points}")
         else:
-            intro = input("(Please enter 1 to enter first room)")
+            print_pause("Invalid input. Please enter '1' or '2'.")
 
 
-### first room function
-def first_room(melee_weapon, color, monster, current_points):
-    print_pause("You enter the first room")
-    print_pause("You see a very terrifying " + color + monster)
-    print_pause("Enter 1 To use your melee weapon to kill it")
-    print_pause("Enter 2 To use escape")
-    ### loop to handle invalid input
-    while True:
-        First_Room = input("(Please enter 1 or 2:")
-        if First_Room == "1":
-            print_pause("You tear the monster to shreds using" + melee_weapon)
-            print_pause("You clear the first room")
-            ### Increasing total points
-            current_points = current_points + 10
-            print_pause("current_points =")
-            print_pause(current_points)
-            second_room1 = input("(Please enter 1 to enter second room)")
-            if current_points == 0:
-               print_pause("game over")
-               print_pause("You suck at this game")
-               play_over()
-               break
-            while True:
-               if second_room1 == "1":
-                    second_room(
-                    melee_weapon,ranged_weapons, color,monster,current_points
-                    )
-                    break
-               else:
-                    second_room1 = input("(Enter 1 to enter second room )")
-        elif First_Room == "2":
-            print_pause("You manage to escape but you were injured")
-            ### decreasing total points
-            current_points = current_points - 10
-            print_pause("current_points =")
-            print_pause(current_points)
-            ### losing condition
-            if current_points == 0:
-               print_pause("game over")
-               print_pause("You suck at this game")
-               play_over()
-               break
-            ###Entering The first room again
-            while True:
-                intro = input("(Please enter 1 to enter first room)")
-                if intro == "1":
-                    first_room(
-                    melee_weapon,color,monster,current_points
-                    )
-                    break
-                else:
-                    intro = input("(Please enter 1 to enter first room)")
-### Secound room function
-def second_room(melee_weapon, ranged_weapons, color, monster, current_points):
-    print_pause("You Enter The second room")
-    ### conditional loop to handle invalid input
-    print_pause("You see a" + color + monster + "at a distance")
-    while True:
-        Second_Room = input("(enter 1to use meleeweapon or 2 to use rangedweapon:")
-        if Second_Room == "1":
-            print_pause(
-               "You run at the monster to kill him using a " + melee_weapon
-            )
-            print_pause("But the" + monster + "spits poison on you")
-            ### decrasing total points
-            current_points = current_points - 10
-            print_pause("current_points=")
-            print_pause(current_points)
-            ### losing condition
-            if current_points == 0:
-               print_pause("game over")
-               print_pause("You suck at this game")
-               play_over()
-               break
-            while True:
-                second_room2 = input("(Enter 1 to enter second room)")
-                if second_room2 == "1":
-                    second_room(
-                    melee_weapon,ranged_weapons,color,monster,current_points
-                    )
-                    break
-                else:
-                    second_room2 = input("(Enter 1 to enter second room)")
-                    break
-        elif Second_Room == "2":
-            print_pause("You use your" + ranged_weapons + "To kill it")
-            ### incrasing total points
-            current_points == current_points + 10
-            print_pause("You clear the second room ")
-            current_points = current_points + 10
-            print_pause("current_points=")
-            print_pause(current_points)
-            print_pause("You have unloucked ultimate attacks")
-            while True:
-                boss_room1 = input("(Please enter 1 to enter boss room)")
-                if boss_room1 == "1":
-                    boss_room(ranged_weapons, color, current_points)
-                    break
-                else:
-                    boss_room1 = input("(Please enter 1 to enter boss room )")
-     
+def second_room(melee_weapon: str, ranged_weapon: str, color: str, monster: str, current_points: int) -> None:
+    """Handles the second room of the game."""
+    print_pause(f"You enter the second room and see a {color} {monster} at a distance.")
+    print_pause("Enter '1' to use melee weapon or '2' to use ranged weapon.")
 
-### boss room function
-def boss_room(ranged_weapons, color, current_points):
-    print_pause("You Enter The third room")
-    print_pause("You see a massive" + color + "Demon")
     while True:
-        ### conditional loop to handle invalid input
-        Input = input(
-            "(Enter 1 to use ultimate attack or 2 to use ranged weapon:"
-        )
-        if Input == "1":
-            print_pause("A black hole swallows the Demon ")
-            print_pause("current points =")
-            ### decrasing total points
-            current_points = current_points + 10
-            print_pause(current_points)
-            points(current_points)
-            play_over()
+        choice = input("Choose an option (1 or 2): ")
+        if choice == "1":
+            print_pause(f"You run at the {monster} with your {melee_weapon}.")
+            print_pause(f"But the {monster} spits poison on you.")
+            current_points = adjust_points(current_points, -10)
+        elif choice == "2":
+            print_pause(f"You use your {ranged_weapon} to kill the {monster}.")
+            current_points += 20
+            print_pause(f"Current points: {current_points}")
+            print_pause("You have unlocked ultimate attacks.")
+
+            while True:
+                next_room = input("Enter '1' to enter the boss room: ")
+                if next_room == "1":
+                    boss_room(ranged_weapon, color, current_points)
+                    break
+        else:
+            print_pause("Invalid input. Please enter '1' or '2'.")
+
+
+def boss_room(ranged_weapon: str, color: str, current_points: int) -> None:
+    """Handles the boss room of the game."""
+    print_pause(f"You enter the boss room and see a massive {color} demon.")
+    print_pause("Enter '1' to use ultimate attack or '2' to use ranged weapon.")
+
+    while True:
+        choice = input("Choose an option (1 or 2): ")
+        if choice == "1":
+            print_pause("A black hole swallows the demon.")
+            current_points += 10
+            print_pause(f"Current points: {current_points}")
+            if current_points >= 60:
+                print_pause("You cleared the game! You are so good at this game!")
+            else:
+                print_pause("You cleared the game. Your gaming skills are pretty average.")
+            end_game()
             break
-        elif Input == "2":
-            print_pause("You use your"+ ranged_weapons + "does not damage it")
-            print_pause(
-               "the demon throws his weapon on you"
-            )
-            ### decrasing total points
-            print_pause("current points =")
-            current_points = current_points - 10
-            print_pause(current_points)
-            ### losing condition
-            if current_points == 0:
-               print_pause("game over")
-               print_pause("You suck at this game")
-               play_over()
-               break
-            while True:
-                boss_room2 = input("(Please enter 1 to enter boss room)")
-                if boss_room2 == "1":
-                    boss_room(ranged_weapons, color, current_points)
-                else:
-                    boss_room2 = input("(Please enter 1 to enter boss room)")
-                    break
-            print_pause(current_points)
-            break
+        elif choice == "2":
+            print_pause(f"You use your {ranged_weapon}, but it does not damage the demon.")
+            print_pause("The demon throws his weapon at you.")
+            current_points = adjust_points(current_points, -10)
+        else:
+            print_pause("Invalid input. Please enter '1' or '2'.")
 
 
-
+# Start the game
 game_intro()
